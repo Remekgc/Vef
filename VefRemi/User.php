1be4ba62f0fa030b6fcd9531de39c91764d6131e
Remekgc <remekgc@gmail.com>
2016-03-10 12:56:40 +0000

    something

--- VefRemi/User.php
+++ VefRemi/User.php
@@ -3,25 +3,11 @@
 <head>
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <title>Foundation | Welcome</title>
+    <title>Login Movie store</title>
     <link rel="stylesheet" href="http://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.min.css">
     <link rel="stylesheet" href="styles.css">
 </head>
-<body><!-- <?php 
-            $MainMynd = "http://tsuts.tskoli.is/2t/2601983359/VefRemi/Myndir/Main.jpg";
-            $bMynd1 = "http://tsuts.tskoli.is/2t/2601983359/VefRemi/Myndir/PointBreak.jpg";
-            $bMynd2 = "http://tsuts.tskoli.is/2t/2601983359/VefRemi/Myndir/Batman.jpg";
-            $bMynd3 = "http://tsuts.tskoli.is/2t/2601983359/VefRemi/Myndir/DeadPool.jpg";
-            $bMynd4 = "http://tsuts.tskoli.is/2t/2601983359/VefRemi/Myndir/StarTrek.jpg";
-            $LbMynd1 = "http://tsuts.tskoli.is/2t/2601983359/VefRemi/Myndir/Taken.jpg";
-            $LbMynd2 = "http://tsuts.tskoli.is/2t/2601983359/VefRemi/Myndir/BatmanOld.jpg";
-            $LbMynd3 = "http://tsuts.tskoli.is/2t/2601983359/VefRemi/Myndir/Robocop.jpg";
-            $LbMynd4 = "http://tsuts.tskoli.is/2t/2601983359/VefRemi/Myndir/Hercules.jpg";
-            $LbMynd5 = "http://tsuts.tskoli.is/2t/2601983359/VefRemi/Myndir/Water.jpg";
-            $LbMynd6 = "http://tsuts.tskoli.is/2t/2601983359/VefRemi/Myndir/Cove.jpg";
-            
-?>
--->
+<body>
 <?php
 
 include 'header.php';
@@ -31,51 +17,81 @@ echo "$header";
 <div class="mother">
 <div class="tabs">
     
-                <div class="tab">
-                    <label for="tab-1"><h3>Login</h3></label>
-       
+                <div class="tab">     
                     <div class="content">
-                        <form class="pure-form">
-                            <fieldset class="pure-group">
-                                <input type="text" class="pure-input-1-2" placeholder="Username">
-                                <input type="text" class="pure-input-1-2" placeholder="Password">
-                                <input type="email" class="pure-input-1-2" placeholder="Email">
-                            </fieldset>
-                            <button type="submit" class="BTNlog">Sign in</button>
-                        </form>
+                         <label for="tab-2"><h3>Login</h3></label>  
+                    <div class="content">
+                    <form method="post" action="">
+
+                    <p>
+
+                    <label for="name">Name:</label>
+
+                    <input name="name" id="name" type="text">
+
+                    </p>
+
+                     <p>
+
+                    <label for="Password">Password:</label>
+
+                    <input name="password" id="password" type="password">
+
+                    </p>
+                    <p>
+
+                    <input name="send" type="submit" value="Login">
+
+                    </p>
                     </div> 
+
                 </div>
     
                 <div class="tab">
-                    <label for="tab-2"><h3>Register</h3></label>               
+                    <label for="tab-2"><h3>Register</h3></label>  
                     <div class="content">
-                        <form class="pure-form pure-form-aligned">
-                            <div class="pure-control-group">
-                                <input id="name" type="text" placeholder="Username" reguired>
-                            </div>
+                    <form method="post" action="">
 
-                            <div class="pure-control-group">
-                                <input id="password" type="password" placeholder="Password" reguired>
-                            </div>
+                    <p>
 
-                            <div class="pure-control-group">
-                                <input id="email" type="email" placeholder="Email Address" reguired>
-                            </div>
+                    <label for="name">Name:</label>
 
-                            <div class="pure-control-group">
-                                <input id="foo" type="text" placeholder="First dog name...">
-                            </div>
+                    <input name="name" id="name" type="text">
 
-                            <div class="pure-controls">
-                                <label for="cb" class="pure-checkbox">
-                                    <input id="cb" type="checkbox"> I've read the terms and conditions
-                                </label>
+                    </p>
 
-                                <button type="submit" class="BTNreg">Register</button>
-                            </div>
-                        </form>
-                    </div> 
+                     <p>
+
+                    <label for="Password">Password:</label>
+
+                    <input name="password" id="password" type="password">
+
+                    </p>
+
+                    <p>
+
+                    <label for="email">Email:</label>
+
+                    <input name="email" id="email" type="email">
+
+                    </p>
+
+                    <p>
+
+                    <input name="send" type="submit" value="Register">
+
+                    </p>
+
+                    </form>
+                    
+                    <pre>
+
+                    <?php if ($_POST) { print_r($_POST); } ?>
+
+                    </pre>
+          
                 </div>
+                </div> 
             </div>
 <?php
 
