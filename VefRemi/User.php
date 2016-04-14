fec893ea0ddba119278de39b3b98d8a63f2fbf7b
Remekgc <remekgc@gmail.com>
2016-04-14 12:46:32 +0000

    Ywolamsdads

--- VefRemi/User.php
+++ VefRemi/User.php
@@ -1,4 +1,29 @@
 <!doctype html>
+<?php
+$errors = [];
+
+if (isset($_POST['register'])) {
+    $firstname = trim($_POST['firstname']);
+    $lastname = trim($_POST['lastname']);
+    $email = trim($_POST['email']);
+    $username = trim($_POST['username']);
+    $password = trim($_POST['password']);
+    require_once 'connection.php';
+    require_once 'Users.php';
+
+    $dbUsers = new Users($conn);
+    print_r($_POST);
+
+  /*$status = $dbUsers->newUser("Rems","Jens","Fens","Helloh","Kemli");*/
+  $status = $dbUsers->newUser($firstname,$lastname,$email,$username,$password);
+
+    if ($status) {
+        $success = "$username has been registered. You may now log in.";
+    }else{
+        $errors[] = "$username is already in use. Please choose another username.";
+    } 
+}
+?>
 <html class="no-js" lang="en">
 <head>
     <meta charset="utf-8"/>
@@ -47,7 +72,6 @@ echo "$header";
                                         unset($_SESSION['registername']);
                                         unset($_SESSION['registerpassword']);
                                         header('Location: http://tsuts.tskoli.is/2t/2601983359/VefRemi/PictureLibary.php');
-                                        echo "Wtf am I doing here";
                                     }
                                 }
                             }
@@ -55,46 +79,44 @@ echo "$header";
                     </div>
                 </div>
     
-                <div class="tab">
-                    <label for="tab-2"><h3>Register</h3></label>  
-                    <div class="content">
-                    <form method="post" action="">
-
-                        <p>
-
-                        <label for="name">Name:</label>
-
-                        <input name="name" id="name" type="text">
-
-                        </p>
-
+                       <h1>Register user</h1>
+                      <?php
+                        if (isset($success)) {
+                            echo "<p>$success</p>";
+                     } elseif (isset($errors) && !empty($errors)) {
+                          echo '<ul>';
+                         foreach ($errors as $error) {
+                              echo "<li>$error</li>";
+                         }
+                         echo '</ul>';
+                     }else{
+                      ?>
+                      <form method="post" action="">
+                          <p>
+                             <label for="username">firstname:</label>
+                             <input type="text" name="firstname" id="firstname" required>
+                         </p>
+                           <p>
+                                <label for="username">lastname:</label>
+                               <input type="text" name="lastname" id="lastname" required>
+                           </p>
+                           <p>
+                              <label for="username">email:</label>
+                             <input type="text" name="email" id="email" required>
+                          </p>
+                           <p>
+                             <label for="username">username:</label>
+                             <input type="text" name="username" id="username" required>
+                          </p>
                          <p>
-
-                        <label for="Password">Password:</label>
-
-                        <input name="password" id="password" type="password">
-
-                        </p>
-
-                        <p>
-
-                        <label for="email">Email:</label>
-
-                        <input name="email" id="email" type="email">
-
-                        </p>
-
-                        <p>
-
-                        <input name="send" type="submit" value="Register">
-
-                        </p>
-
-                    </form>
-                        
-                        <pre>
-
-                        <?php 
+                                <label for="pwd">Password:</label>
+                                <input type="password" name="password" id="password" required>
+                         </p>
+         
+                         <p>
+                             <input name="register" type="submit" id="register" value="Register">
+                            </p>
+                            <?php 
                             if ($_POST) { print_r($_POST); } 
                             if(isset($_POST['name']))
                             {
@@ -102,11 +124,9 @@ echo "$header";
                                 $_SESSION['registerpassword'] = $_POST["password"];
                             }
                         ?>
-
-                        </pre>
-          
-                </div>
-                </div> 
+                      </form>
+                      <?php } ?>
+                        
             </div>
 <?php
 
